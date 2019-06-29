719adaa5dd634b57e239a67fde2c4e7fffd9a29d
Tom Robinson <tom6rr@aol.com>
2019-06-29 14:10:27 +0100

    started on basic home page

---
+++ src/components/MovieBanner.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class MovieBanner extends Component {
+  state = {
+    movies: null
+  };
+
+  componentDidMount() {
+    console.log('hi')
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import MovieBanner from '../MovieBanner'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <MovieBanner />
+      </div>
+    )
+  }
+}
