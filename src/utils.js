95c5e5270c86c209cc1da0dff7144b4d4a169e8f
Tom Robinson <tom6rr@aol.com>
2019-07-05 21:37:34 +0100

    added carousel to home page showing movies playing now

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import apiKey from './config'
+import { lookup } from 'dns';
 
 const baseUrl = 'https://api.themoviedb.org/3/movie';
 
@@ -7,3 +8,15 @@ export const getLatestMovies = () => {
   return axios.get(`${baseUrl}/upcoming?api_key=${apiKey}&language=en-US&page=1`)
     .then(({ data: { results } }) => results)
 };
+
+export const getGenres = () => {
+  return axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+    .then(({ data: { genres } }) => {
+      return genres.reduce((lookUp, genre) => {
+        lookUp[`${genre.id}`] = genre.name;
+        return lookUp;
+      }, {})
+    })
+};
+
+
